95749adccd3946bedf9de8753b380bea6878f349
tfitts <tfitts@gmail.com>
2020-02-16 00:14:11 -0700

    Remove Verify Token

--- app/controllers/quickbooks_web_connector/quickbooks_web_connector_controller.rb
+++ app/controllers/quickbooks_web_connector/quickbooks_web_connector_controller.rb
@@ -1,5 +1,4 @@
 module QuickbooksWebConnector
   class QuickbooksWebConnectorController < QuickbooksWebConnector.config.parent_controller.constantize
-    skip_before_action :verify_authenticity_token
   end
 end
